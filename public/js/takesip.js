035cc4518fbdac1f7068615f35d009c08eea5423
sophie-truong <59684084+sophie-truong@users.noreply.github.com>
2020-02-27 22:22:08 -0800

    Add files via upload

---
+++ public/js/takesip.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	console.log("oi");
+	initializePage();
+});
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	console.log("yes");
+	$.get("https://a8-prototype.herokuapp.com/user", updateImage);
+}
+
+/*
+ * Make an AJAX call to retrieve project details and add it in
+ */
+function updateImage(result) {
+	console.log(result);
+	var countNumber = result["growth"];
+	var projectHTML = "<img src='/images/Cup" + countNumber + ".png' style='width:40%' ... />";
+	$('.sip-tracker').html(projectHTML);
+}
